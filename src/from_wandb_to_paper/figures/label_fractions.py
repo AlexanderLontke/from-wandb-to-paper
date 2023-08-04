8538d194991c86153a4c2dd61fa04d805692fbe2
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-08-04 11:40:10 +0200

    Added optional label transform

--- src/from_wandb_to_paper/figures/label_fractions.py
+++ src/from_wandb_to_paper/figures/label_fractions.py
@@ -1,19 +1,22 @@
-from typing import Any, Dict, List, Tuple
+from typing import Callable, List, Optional
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 
 
 def get_label_fraction_figure(
-    lf_metrics_table: pd.DataFrame,
-    experiment_names: List[str],
-    label_fractions: List,
-    metric_key: str,
-    aggr_type: str = "mean",
-    error_type: str = "std",
-    name_suffix: str = "",
+        lf_metrics_table: pd.DataFrame,
+        experiment_names: List[str],
+        label_fractions: List,
+        metric_key: str,
+        aggr_type: str = "mean",
+        error_type: str = "std",
+        name_suffix: str = "",
+        label_transform: Optional[Callable[[str], str]] = None,
 ):
+    if label_transform is None:
+        def label_transform(x):
+            return x
     statistics = [aggr_type, error_type]
     graph_dict = {
         name: {stat: [] for stat in statistics} for name in experiment_names
@@ -27,16 +30,15 @@ def get_label_fraction_figure(
                         lf_metrics_table.loc[metric_key, (whole_name, stat)]
                     )
     for name, y_values in graph_dict.items():
-        if len(y_values) > 0:
+        if len(y_values[aggr_type]) > 0:
             g = sns.lineplot(
                 x=label_fractions,
                 y=y_values[aggr_type],
                 markers=True,
-                label=name,
+                label=label_transform(name),
             )
             g.plot(
                 np.asarray([[x, x] for x in label_fractions]).T,
                 np.asarray([[y, y] for y in y_values[error_type]]).T,
             )
             g.set_xticks(label_fractions)
-
