2ea1af5243ae22ceee1f6469692ff2d663acd586
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-08-07 16:35:01 +0200

    Added option to specify highlight mode

--- src/from_wandb_to_paper/latex/tables.py
+++ src/from_wandb_to_paper/latex/tables.py
@@ -1,18 +1,20 @@
+from typing import Literal
 import pandas as pd
 
 
 def metrics_table_to_latex(
     metrics_table: pd.DataFrame,
+    mode: Literal["min", "max"],
     precision: int = 2,
     hrules: bool = True,
     clines: str = "all;data",
-    highlight_max_axis: int = 1,
+    highlight_axis: int = 1,
 ) -> str:
     idx = pd.IndexSlice
     # Set style
-    s = metrics_table.style.highlight_max(
+    s = getattr(metrics_table.style, f"highlight_{mode}")(
         subset=(idx[:], idx[:, "mean"]),
-        axis=highlight_max_axis,
+        axis=highlight_axis,
         props="textbf:--rwrap;",
     ).format(precision=precision)
     return s.to_latex(
