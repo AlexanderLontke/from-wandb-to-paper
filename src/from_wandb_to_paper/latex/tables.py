908ed298dd2bb715560a0564b15e4abee204db5e
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-09-14 16:36:11 +0200

    Added second highlighting

--- src/from_wandb_to_paper/latex/tables.py
+++ src/from_wandb_to_paper/latex/tables.py
@@ -1,5 +1,14 @@
 from typing import Literal
+from functools import partial
+
 import pandas as pd
+import numpy as np
+
+
+def highlight_second_mode(a, mode: Literal["min", "max"], props=""):
+    first_max = getattr(a, mode)()
+    second_max = getattr(a[a != first_max], mode)()
+    return np.where(a == second_max, props, "")
 
 
 def metrics_table_to_latex(
@@ -12,11 +21,18 @@ def metrics_table_to_latex(
 ) -> str:
     idx = pd.IndexSlice
     # Set style
+    # Highlight highest/lowest values
     s = getattr(metrics_table.style, f"highlight_{mode}")(
         subset=(idx[:], idx[:, "mean"]),
         axis=highlight_axis,
         props="textbf:--rwrap;",
     ).format(precision=precision)
+    s = s.apply(
+        partial(highlight_second_mode, mode=mode),
+        axis=highlight_axis,
+        props="textit:--rwrap;",
+    )
+    # Highlight second highest/lowest values
     return s.to_latex(
         column_format="l" + "|rr" * int(len(metrics_table.columns) / 2),
         hrules=hrules,
